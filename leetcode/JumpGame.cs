ad5e84448fb9a21ff9f3e5ff6775e8e409d9b7b5
rohanharikr <rohanharikumar80@gmail.com>
2024-02-09 00:27:04 +0530

    remove log

--- leetcode/JumpGame.cs
+++ leetcode/JumpGame.cs
@@ -4,11 +4,9 @@ public class Solution {
     public bool CanJump(int[] nums) {
         int nearest = nums.Length - 1;
         for(int i = nums.Length - 1; i >= 0; i--)
-        {
-            Console.WriteLine(i + nums[i]);
             if(i + nums[i] >= nearest)
                 nearest = i;
-        }
+        
         if(nearest == 0)
             return true;
         
